c9b1d443cf08127651675a3ad73d78cb7c6814b7
Melody Wong <mwong@dwavesys.com>
2019-05-31 14:37:43 -0700

    Add code to handle 16 by 16 sudoku

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,5 +1,6 @@
 import dimod
 import math
+import string
 
 from dimod.generators.constraints import combinations
 from hybrid.reference import KerberosSampler
@@ -21,7 +22,7 @@ def get_matrix(filename):
         new_line = line.rstrip()    # Strip whitespace
 
         if new_line:
-            new_line = list(map(int, new_line.split(' ')))
+            new_line = list(new_line.split(' ')) #TODO: to upper
             lines.append(new_line)
 
     return lines
@@ -30,17 +31,20 @@ def get_matrix(filename):
 def is_correct(matrix):
     n = len(matrix)        # Number of rows/columns
     m = int(math.sqrt(n))  # Number of subsquare rows/columns
-    solution = set(range(1, n+1))  # Digits in a solution
+    digits = list(string.digits + string.ascii_uppercase)[1:n+1]
+    solution = set(digits)  # Digits in a solution
 
     # Verifying rows
     for row in matrix:
         if set(row) != solution:
+            print("Error in row: ", row)
             return False
 
     # Verifying columns
     for j in range(n):
         col = [matrix[i][j] for i in range(n)]
         if set(col) != solution:
+            print("Error in col: ", col)
             return False
 
     # Verifying subsquares
@@ -50,19 +54,21 @@ def is_correct(matrix):
             subsquare = [matrix[i + r_scalar * m][j + c_scalar * m] for i, j
                          in subsquare_coords]
             if set(subsquare) != solution:
+                print("Error in subsquare: ", subsquare)
                 return False
 
     return True
 
 
+#TODO pass keyword args to kerberos
 def main():
-    filename = "problem.txt"
+    filename = "problem_16x16.txt"
     matrix = get_matrix(filename)
 
     # Set up
     n = len(matrix)          # Number of rows/columns in sudoku
     m = int(math.sqrt(n))    # Number of rows/columns in sudoku subsquare
-    digits = range(1, n+1)
+    digits = list(string.digits + string.ascii_uppercase)[1:n+1]
 
     bqm = dimod.BinaryQuadraticModel({}, {}, 0.0, dimod.SPIN)
 
@@ -89,7 +95,7 @@ def main():
 
     # Constraint: Each sub-square cannot have duplicates
     # Build indices of a basic subsquare
-    subsquare_indices = [(row, col) for row in range(3) for col in range(3)]
+    subsquare_indices = [(row, col) for row in range(m) for col in range(m)]
 
     # Build full sudoku array
     for r_scalar in range(m):
@@ -108,11 +114,11 @@ def main():
     # Constraint: Fix known values
     for row, line in enumerate(matrix):
         for col, value in enumerate(line):
-            if value > 0:
+            if value != '0':
                 bqm.fix_variable(get_label(row, col, value), 1)
 
     # Solve BQM
-    solution = KerberosSampler().sample(bqm, max_iter=10, convergence=3)
+    solution = KerberosSampler().sample(bqm, max_iter=40, convergence=6)
     best_solution = solution.first.sample
 
     # Print solution
@@ -121,10 +127,11 @@ def main():
     for label in solution_list:
         coord, digit = label.split('_')
         row, col = map(int, coord.split(','))
-        matrix[row][col] = int(digit)
+        matrix[row][col] = digit
 
     for line in matrix:
-        print(line)
+        printable_line = " ".join(line)
+        print(printable_line)
 
     # Verify
     if is_correct(matrix):
